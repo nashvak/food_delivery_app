c7ae5c4ea0c1e54777119a0ee18329f78ecc7fb9
nashva <nashvakizhakkayil@gmail.com>
2023-12-19 22:02:24 +0530

    working on login screen

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const appColor = Color(0xFF419F7D);
+const primaryColor = Color(0xFFF2F2F2);
--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/colors/colors.dart';
 
 class ScreenSignup extends StatelessWidget {
   const ScreenSignup({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Stack(
+        children: [
+          Positioned(
+            child: Container(
+              width: double.infinity,
+              height: 300,
+              color: appColor,
+            ),
+          ),
+          Positioned(
+            top: 200,
+            left: 20,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 150,
+                      color: Colors.amber,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
