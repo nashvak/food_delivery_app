2848ae9f4bfd71bbd7c592e8e5ac289dfb0d3df3
nashva <nashvakizhakkayil@gmail.com>
2024-01-03 20:15:09 +0530

    enable location screen ui completed

--- lib/enable_location/enable_location.dart
+++ lib/enable_location/enable_location.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/constantss/colors.dart';
 import 'package:food_delivery_app/constantss/sizedbox.dart';
+import 'package:food_delivery_app/signup_screen/widgets/button.dart';
+import 'package:lottie/lottie.dart';
 
 class EnableLocation extends StatelessWidget {
   const EnableLocation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Kheight(value: 100),
-          Text('Set your location to start exploring restaurants near you'),
-        ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(
+              flex: 3,
+            ),
+            const Text(
+              'Set your location to start exploring restaurants near you',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            ),
+            const Kheight(value: 30),
+            Lottie.asset('images/lottie/lottie_location.json',
+                fit: BoxFit.cover),
+            const Spacer(
+              flex: 3,
+            ),
+            MainButton(title: 'Enable Device location', ontap: () {}),
+            const Kheight(value: 10),
+            const Text(
+              'Set location manually',
+              style: TextStyle(fontSize: 18, color: appColor),
+            ),
+            const Spacer(
+              flex: 1,
+            ),
+          ],
+        ),
       ),
     );
   }
